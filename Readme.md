4acf2932677a112865fa7a6dc6b387fb7299ff05
Loganathan Sellappa <{ID}+{username}@users.noreply.github.com>
2024-10-01 23:00:26 +0200

    Added projects with clean code architecture and non working Extensions for DB commection

---
+++ Readme.md
@@ -0,0 +1,4 @@
+dotnet new webapi -n Restuarants.API --no-openapi -controllers
+
+dotnet sln add ./Restuarants.API
+Project `Restuarants.API/Restuarants.API.csproj`
---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+internal class RestaurantsDbContext : DbContext 
+{
+    internal DbSet<Restaurant> Restaurants { get; set; }
+    internal DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost;Database=Restaurants;Trusted_Connection=True;");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>()
+            .OwnsOne(r => r.Address);
+
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes)
+            .WithOne()
+            .HasForeignKey(d => d.RestaurantId);
+
+    }
+}
