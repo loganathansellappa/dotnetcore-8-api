8d2aeb9ba44064273abd7aa2f0988d83e541c6ef
Loganathan Sellappa <{ID}+{username}@users.noreply.github.com>
2024-10-28 23:03:10 +0100

    Moved service mapping in to separate class

---
+++ Restaurants.API/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,63 @@
+using Microsoft.OpenApi.Models;
+using Restaurants.API.Controllers;
+using Restaurants.API.Middlewares;
+using Restaurants.Application.Extensions;
+using Restaurants.Infrastructure.Extensions;
+using Serilog;
+
+namespace Restaurants.API.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static void AddPresentation(this WebApplicationBuilder builder)
+    {
+                
+        // Add services to the container.
+
+        builder.Services.AddEndpointsApiExplorer();
+
+
+        builder.Services.AddControllers();
+        builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
+        builder.Services.AddInfrastructure(builder.Configuration);
+        builder.Services.AddApplication();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http, 
+                Scheme = "Bearer"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "bearerAuth"
+                        }
+                    },
+                    []
+                }
+            });
+        });
+        builder.Services.AddScoped<ErrorHandlingMiddleware>();
+        builder.Services.AddScoped<LoggingMiddleware>();
+        builder.Host.UseSerilog((context, configuration) =>
+        {
+            configuration.ReadFrom.Configuration(context.Configuration);
+            /*
+             * Move to appsettings.json
+             */
+            //.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            //.MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
+            //.Enrich.FromLogContext()
+            //.WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day, outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}");
+            //.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
+            //
+        });
+    }
+}
--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,65 +1,12 @@
-using Microsoft.OpenApi.Models;
-using Restaurants.API.Controllers;
+using Restaurants.API.Extensions;
 using Restaurants.API.Middlewares;
-using Restaurants.Application.Extensions;
 using Restaurants.Domain.Entities;
-using Restaurants.Domain.Repositories;
-using Restaurants.Infrastructure;       
-using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
-using Serilog.Events;using Serilog.Formatting.Compact;
-using Serilog.Formatting.Json;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.AddPresentation();
 
-// Add services to the container.
-
-builder.Services.AddEndpointsApiExplorer();
-
-
-builder.Services.AddControllers();
-builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddApplication();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http, 
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "bearerAuth"
-                }
-            },
-            []
-        }
-    });
-});
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<LoggingMiddleware>();
-builder.Host.UseSerilog((context, configuration) =>
-{
-    configuration.ReadFrom.Configuration(context.Configuration);
-    /*
-     * Move to appsettings.json
-     */
-    //.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-    //.MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
-    //.Enrich.FromLogContext()
-    //.WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day, outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}");
-    //.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
-    //
-});
 var app = builder.Build();
 var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
