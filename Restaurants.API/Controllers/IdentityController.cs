4941bc71073db4dd4ca940d390ca754f2a7ea145
Loganathan Sellappa <{ID}+{username}@users.noreply.github.com>
2024-10-30 23:31:44 +0100

    Added DELETE endpoint to unassign user roles

--- Restaurants.API/Controllers/IdentityController.cs
+++ Restaurants.API/Controllers/IdentityController.cs
@@ -2,6 +2,7 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Users.Commands.AssignUserRole;
+using Restaurants.Application.Users.Commands.UnAssignUserRole;
 using Restaurants.Application.Users.Commands.UpdateUser;
 using Restaurants.Domain.Constants;
 
@@ -19,9 +20,17 @@ public class IdentityController(IMediator mediator) : ControllerBase
     }
     [HttpPost("userRole")]
     [Authorize(Roles = UserRoles.Admin)]
-    public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand assignUserRoleCommand)
+    public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
     {
-        await mediator.Send(assignUserRoleCommand);
+        await mediator.Send(command);
+        return NoContent();
+    }
+    
+    [HttpDelete("userRole")]
+    [Authorize(Roles = UserRoles.Admin)]
+    public async Task<IActionResult> UnAssignUserRole(UnAssignUserRoleCommand command)
+    {
+        await mediator.Send(command);
         return NoContent();
     }
 }
