9353e6c91587c2dfb5582cba9edbb3ec9a67fb39
Loganathan Sellappa <{ID}+{username}@users.noreply.github.com>
2024-11-05 21:03:02 +0100

    Added custom claim requirements

--- Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -6,6 +7,7 @@ using Restaurants.Domain.Constants;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 using Restaurants.Infrastructure.Authorization;
+using Restaurants.Infrastructure.Authorization.Requirements;
 using Restaurants.Infrastructure.Persistence;
 using Restaurants.Infrastructure.Repositories;
 using Restaurants.Infrastructure.Seeders;
@@ -29,8 +31,9 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IRestaurantsRepository, RestaurantsRepository>();
         services.AddScoped<IDishesRepository, DishesRepository>();
         services.AddAuthorizationBuilder()
-            .AddPolicy("HasNationality", builder => builder.RequireClaim("Nationality", "Indian", "Deutch"));
-        
-
+            .AddPolicy(PolicyNames.HasNationality, 
+                builder => builder.RequireClaim(AppClaimTypes.Nationality, "Indian", "Deutch"))
+            .AddPolicy(PolicyNames.Atleast20, builder => builder.AddRequirements(new MinimumAgeRequirement(20)));
+        services.AddScoped<IAuthorizationHandler, MinimumAgeRequirementHandler>();
     }
 }
