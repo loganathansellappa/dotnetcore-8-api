69c56b53a55326d974a9654c9ce2f3f2ddfd1486
Loganathan Sellappa <{ID}+{username}@users.noreply.github.com>
2024-11-05 22:06:06 +0100

    Updated create restaurant api to store woenr details

--- Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
+++ Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Restaurants.Application.Users;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 
@@ -8,13 +9,14 @@ namespace Restaurants.Application.Restaurants.Commands.CreateRestaurant;
 
 public class CreateRestaurantCommandHandler(ILogger<CreateRestaurantCommandHandler> logger, 
     IMapper mapper,
-    IRestaurantsRepository restaurantsRepository) : IRequestHandler<CreateRestaurantCommand, int>
+    IRestaurantsRepository restaurantsRepository, IUserContext userContext) : IRequestHandler<CreateRestaurantCommand, int>
 {
     public async Task<int> Handle(CreateRestaurantCommand request, CancellationToken cancellationToken)
     {
         logger.LogInformation($"Creating restaurant");
-
+        var currentUser = userContext.GetCurrentUser()!;
         var restaurant = mapper.Map<Restaurant>(request);
+        restaurant.OwnerId = currentUser.Id;
         await restaurantsRepository.CreateAsync(restaurant);
         return restaurant.Id;
     }
