19aab77e39aa956e0d47703d324030e4aa002b76
Loganathan Sellappa <{ID}+{username}@users.noreply.github.com>
2024-11-25 22:53:32 +0100

    gitigemore updated

--- src/Restaurants.Infrastructure/Storage/BlobStorageService.cs
+++ src/Restaurants.Infrastructure/Storage/BlobStorageService.cs
@@ -1,4 +1,5 @@
 using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
 using Microsoft.Extensions.Options;
 using Restaurants.Domain.Interfaces;
 using Restaurants.Infrastructure.Configuration;
@@ -16,5 +17,33 @@ public class BlobStorageService(IOptions<BlobStorageSetting> blobStorageSettingO
         await blocClient.UploadAsync(stream);
         var blobUri = blocClient.Uri.ToString();
         return blobUri; 
+    }
+    
+    public string GetBlobUri(string? blobUrl)
+    {
+        if (blobUrl == null) return string.Empty;
+        var blobServiceClient = new BlobServiceClient(_blobStorageSetting.ConnectionString);
+        var sasBuilder = new BlobSasBuilder
+        {
+            BlobContainerName = _blobStorageSetting.ContainerName,
+            BlobName = GetBlobNameFromUrl(blobUrl),
+            Resource = "b",
+            StartsOn = DateTimeOffset.UtcNow,
+            ExpiresOn = DateTimeOffset.UtcNow.AddDays(1)
+        };
+        
+        sasBuilder.SetPermissions(BlobSasPermissions.Read);
+        var sasToken = sasBuilder.ToSasQueryParameters(new Azure.Storage.StorageSharedKeyCredential(blobServiceClient.AccountName, _blobStorageSetting.AccountKey)).ToString();
+        if (string.IsNullOrWhiteSpace(sasToken))
+        {
+            return string.Empty;
+        }
+        return $"{blobUrl}?{sasToken}";
+    }
+
+    private string GetBlobNameFromUrl(string url)
+    {
+        var uri = new Uri(url);
+        return uri.Segments.Last();
     }
 }
